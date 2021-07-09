634bc5c46c8f6a3452cba529be1c914582553fa9
milkshakebrain <87139266+milkshakebrain@users.noreply.github.com>
2021-07-08 20:25:01 -0700

    lemonade bar

--- Co-op Experimentations/Assets/Scripts/CollectorScript.cs
+++ Co-op Experimentations/Assets/Scripts/CollectorScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectorScript : MonoBehaviour
 {
@@ -7,16 +8,49 @@ public class CollectorScript : MonoBehaviour
     bool CanPress = true;
     public float cooldownTime = 1f;
     float nextClickTime = 0f;
-    
+    public Slider ProgressSlider;
+    public bool givenMoney = false;
+    private float timerSpeed = 1f;
+    private float elapsed;
+    public class CountingTimer : MonoBehaviour { }
+
+
+    public void Update()
+    {
+
+        if (Time.time > nextClickTime && !givenMoney)
+        {
+            totalBalance++;
+            givenMoney = true;
+        }
+
+        //write code here corbiee worbieeee
+        if (givenMoney==false)
+            elapsed += Time.deltaTime;
+
+
+        if (elapsed >= timerSpeed)
+        {
+            elapsed = 0f;
+        }
+
+        ProgressSlider.value = elapsed;
+
+    }
+
     public void OnClick()
     {
-        if(Time.time > nextClickTime) {
+        if (Time.time > nextClickTime)
+        {
             ExecuteClick();
         }
     }
+
     void ExecuteClick()
     {
-        totalBalance++;
-        nextClickTime = Time.time + cooldownTime;
-    }
+            //totalBalance++;
+            nextClickTime = Time.time + cooldownTime;
+            givenMoney = false;
+
+    }  
 }
